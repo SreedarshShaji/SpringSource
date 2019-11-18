58bef83d49ea64277f158fa9313e02a2adbe6e98
sreedarsh <sreedarsh@E6220>
2019-11-18 20:41:55 +0530

    First trial Commit

---
+++ Spring3_Spring_Core/src/main/java/com/spring/start/Spring3_Spring_Core/Alien.java
@@ -0,0 +1,8 @@
+package com.spring.start.Spring3_Spring_Core;
+
+public class Alien {
+	 	public void code()
+	 	{
+	 		System.out.println("Hey there, I am coding");
+	 	}
+}
---
+++ Spring3_Spring_Core/src/main/java/com/spring/start/Spring3_Spring_Core/App.java
@@ -0,0 +1,9 @@
+package com.spring.start.Spring3_Spring_Core;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Alien obj;
+    }
+}
