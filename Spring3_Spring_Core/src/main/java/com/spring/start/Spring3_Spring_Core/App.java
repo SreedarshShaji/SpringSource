9a08ed24185a9889fe62eeb73bc720e91e1f201c
sreedarsh <sreedarsh@E6220>
2019-11-20 00:17:38 +0530

    Fixed with class path xml application context.

--- Spring3_Spring_Core/src/main/java/com/spring/start/Spring3_Spring_Core/App.java
+++ Spring3_Spring_Core/src/main/java/com/spring/start/Spring3_Spring_Core/App.java
@@ -1,9 +1,20 @@
 package com.spring.start.Spring3_Spring_Core;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
 public class App 
 {
     public static void main( String[] args )
     {
-    	Alien obj;
+		ApplicationContext factory=
+    			new ClassPathXmlApplicationContext("/com/spring/start/Spring3_Spring_Core/Spring.xml");
+    	
+    	
+    	Alien obj=(Alien)factory.getBean("alien");
+    	obj.code();
     }
 }
